ca5c32f6e2385fcb2b4fa745f4cc6b08e9e65c41
Altif123 <altifali.aa@gmail.com>
2019-03-08 16:51:15 +0000

    updated

--- RPG1/Character.h
+++ RPG1/Character.h
@@ -2,7 +2,36 @@
 class Character
 {
 public:
+
+
 	Character();
 	virtual ~Character();
-};
 
+	//methods
+	
+
+
+	//getters 
+
+
+
+	const int& getHp() const {//creates reference to the vairaible hp
+		return this->hp;
+	}
+	
+	const int& getSheild() const {//creates reference to the vairaible attack oints 
+		return this->sheild;
+	}
+	
+
+	//setters
+
+
+private:
+
+	
+	int hp;
+	int maxhp;
+	int sheild;
+
+};
